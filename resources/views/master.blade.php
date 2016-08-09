e97ac61fe95a75bb060d2e6626dfb207353db698
TJ McKenzie <mckenziet@gmail.com>
2016-08-08 21:17:34 -0400

    removes unneeded line

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -18,7 +18,6 @@
 
         <link rel="stylesheet" href="{{ url('css/app.css') }}">
 
-
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
         <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
         <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
